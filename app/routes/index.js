d33e5bd694ef53af5c5eb8f54ded494837ea306b
RobL <robinswaylu@gmail.com>
2016-01-07 09:41:45 +0000

    beta

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,37 +1,21 @@
 'use strict';
 
-var path = process.cwd(); 
-var moment = require('moment');
+var path = process.cwd();  
+var bodyParser = require('body-parser');
 
 module.exports = function (app, passport) {
     
     app.get('/', function (req, res) {
-  		res.sendFile(path + '/public/index.html');
-	});
-   
-    app.get('/:date', function (req, res) {
-  		var answer = {
-  			unix: null,
-			natural: null
-  		}, momentObj;
- 
-  		//if params is unix or natural. condition(contains only numbers or conatins months)
-  		// only number regex ^[0-9]+$
-  		//var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-  		if(/^[0-9]+$/.test(req.params.date) && req.params.date > 0){ 
-  			momentObj = moment.unix(req.params.date);
-  			answer.unix = parseInt(req.params.date);
-  			answer.natural = momentObj.format('MMMM D, YYYY');
-  			res.json(answer);
-  		}else if(moment(req.params.date, ['MMMM D YYYY', 'MMMM D, YYYY'], true).isValid()){
-  			momentObj = moment(req.params.date);
-  			answer.unix = momentObj.unix();
-  			answer.natural = momentObj.format('MMMM D, YYYY');
-  			res.json(answer);
-  		}
-  		//else if params.date is not what we want, then we return null
-  		else{
-  			res.json(answer);
-  		} 
+        
+        var ip = req.headers['x-forwarded-for'],
+        lang = req.headers['accept-language'].split(",")[0],
+        software = req.headers['user-agent'].match(/\(([^)]+)\)/),
+        whoAmI = {
+            ipaddress: ip,
+            language: lang,
+            software: software[1]
+        }
+        res.render('index', whoAmI);
+  	//	res.json(whoAmI);
 	});
 };
--- server.js
+++ server.js
@@ -2,10 +2,16 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
+var bodyParser = require('body-parser');
 
 var app = express();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.use('/public', express.static(process.cwd() + '/public')); 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 routes(app);
 
