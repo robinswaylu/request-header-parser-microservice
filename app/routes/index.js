098963f9a21dd9358e3f1ea5d0766aa04ade8e19
RobL <robinswaylu@gmail.com>
2016-01-07 05:29:37 +0000

    modify routing to accept both natural date format and unix number, then display converted json object to client

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,37 @@
 'use strict';
 
-var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var path = process.cwd(); 
+var moment = require('moment');
 
 module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
-
-	app.route('/')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/index.html');
-		});
-
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
-		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+    
+    app.get('/', function (req, res) {
+  		res.sendFile(path + '/public/index.html');
+	});
+   
+    app.get('/:date', function (req, res) {
+  		var answer = {
+  			unix: null,
+			natural: null
+  		}, momentObj;
+ 
+  		//if params is unix or natural. condition(contains only numbers or conatins months)
+  		// only number regex ^[0-9]+$
+  		//var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+  		if(/^[0-9]+$/.test(req.params.date) && req.params.date > 0){ 
+  			momentObj = moment.unix(req.params.date);
+  			answer.unix = parseInt(req.params.date);
+  			answer.natural = momentObj.format('MMMM D, YYYY');
+  			res.json(answer);
+  		}else if(moment(req.params.date, ['MMMM D YYYY', 'MMMM D, YYYY'], true).isValid()){
+  			momentObj = moment(req.params.date);
+  			answer.unix = momentObj.unix();
+  			answer.natural = momentObj.format('MMMM D, YYYY');
+  			res.json(answer);
+  		}
+  		//else if params.date is not what we want, then we return null
+  		else{
+  			res.json(answer);
+  		} 
+	});
 };
